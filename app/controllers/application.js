1cab17a5e7cce72717b97ce11638322ea8cb982e
Andy Mention <amention@gmail.com>
2014-11-24 11:01:36 -0700

    Tipping calculator MVP

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  billAmount: 0,
+  tipPercentage: 18,
 
+  tipAsPercentage: function(){
+    return this.get('tipPercentage') / 100;
+  }.property('tipPercentage'),
+
+  tipAmount: function(){
+    return this.get('tipAsPercentage') * this.get('billAmount');
+  }.property('tipAsPercentage', 'billAmount'),
+ 
+  totalBill: function(){
+               return parseInt(this.get('billAmount'), 10) + this.get('tipAmount');
+             }.property('tipAmount', 'billAmount')
 
 });
